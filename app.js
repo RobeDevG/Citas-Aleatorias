8668d69c0e8b3575096de8b82675cc3d7a5aeb69
Roberto <robertodev97@gmail.com>
2023-09-16 19:45:22 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+let botonElem = document.getElementById('boton-cambiar-de-cita');
+let citaElem = document.getElementById('cita');
+let autorElem = document.getElementById('autor');
+
+function generarEnteroAleatorio(max) {
+ 
+  return Math.floor(Math.random() * max);
+}
+function cambiarCita() {
+  let indiceAleatorio = generarEnteroAleatorio(citas.length);
+  citaElem.innerText = `"${citas[indiceAleatorio].texto}"`;
+  autorElem.innerText = citas[indiceAleatorio].autor;
+}
+cambiarCita();
+
+botonElem.addEventListener('click', cambiarCita);
+
---
+++ citas.js
@@ -0,0 +1,58 @@
+let citas = [
+  {
+    'autor':'Albert Einsen',
+    'texto' : 'Valla pa la pvvvvnga',
+  },
+  {
+    'autor':'Chocolate MC',
+    'texto' : 'Rastameba 2',
+  },
+  {
+    'autor':'Roberto',
+    'texto' : 'Ay dias q tiras la carnada y pica al momento, dias q demora,y dias q no pica ,no estoy hablando de pescar',
+  },
+  {
+    'autor':'greco',
+    'texto' : 'bueno ni idea',
+  },
+  {
+    'autor':'hola',
+    'texto' : 'Valla pa la pinga',
+  },
+  {
+    'autor':'buuu',
+    'texto' : 'Valla pa la pinga',
+  },
+  {
+    'autor':'oro',
+    'texto' : 'Valla pa la pinga',
+  },
+  {
+    'autor':'Albert Einsen',
+    'texto' : 'tomate esto',
+  },
+  {
+    'autor':'Albert Einsen',
+    'texto' : 'lo de la relatividad y eso',
+  },
+  {
+    'autor':'Albert Einsen',
+    'texto' : 'te salgo hasta en la sopa',
+  },
+  {
+    'autor':'d',
+    'texto' : 'Ay dias q tiras la carnada y pica al momento, dias q demora,y dias q no pica ,no estoy hablando de pescar',
+  },
+  {
+    'autor':'c',
+    'texto' : 'Ay dias q tiras la carnada y pica al momento, dias q demora,y dias q no pica ,no estoy hablando de pescar',
+  },
+  {
+    'autor':'cc',
+    'texto' : 'Ay dias q tiras la carnada y pica al momento, dias q demora,y dias q no pica ,no estoy hablando de pescar',
+  },
+  {
+    'autor':'ccc',
+    'texto' : 'Ay dias q tiras la carnada y pica al momento, dias q demora,y dias q no pica ,no estoy hablando de pescar',
+  },
+]
