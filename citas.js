1bea96c5ee4fb19d114c0b731e527d63d246e8e6
RobeDevG <robertodev97@gmail.com>
2023-11-02 15:33:57 -0400

    ActualizandoCitas

--- citas.js
+++ citas.js
@@ -1,59 +1,59 @@
 let citas = [
   {
-    'autor': 'Albert Einsen',
-    'texto': 'Valla pa la pvvvvnga',
+    'autor': 'Mahatma Gandhi',
+    'texto': 'No hay caminos para la paz, la paz es el camino ',
   },
   {
-    'autor': 'Chocolate MC',
-    'texto': 'Rastameba 2',
+    'autor': 'Charles Baudelaire',
+    'texto': 'Para trabajar basta estar convencido de una cosa: que trabajar es menos aburrido que divertirse',
   },
   
   {
-    'autor': 'Roberto',
-    'texto': 'Ay dias q tiras la carnada y pica al momento, dias q demora,y dias q no pica ,no estoy hablando de pescar',
+    'autor': 'Jacinto Benavente',
+    'texto': 'Lo peor que hacen los malos es obligarnos a dudar de los buenos ',
   },
   {
-    'autor': 'greco',
-    'texto': 'bueno ni idea',
+    'autor': 'Bob Marley',
+    'texto': 'Las guerras seguirán mientras el color de la piel siga siendo más importante que el de los ojos',
   },
   {
-    'autor': 'hola',
-    'texto': 'Valla pa la pinga',
+    'autor': 'Meryl Streep',
+    'texto': 'El poder no cambia a las personas, solo revela quiénes son realmente',
   },
   {
-    'autor': 'buuu',
-    'texto': 'Valla pa la pinga',
+    'autor': 'Abraham Lincoln',
+    'texto': 'Ningún hombre es lo bastante bueno para gobernar a otros sin su consentimiento',
   },
   {
-    'autor': 'oro',
-    'texto': 'Valla pa la pinga',
-  },
-  {
-    'autor': 'Albert Einsen',
-    'texto': 'tomate esto',
+    'autor': 'Aristóteles',
+    'texto': 'El sabio no dice nunca todo lo que piensa, pero siempre piensa todo lo que dice',
   },
   {
     'autor': 'Albert Einsen',
-    'texto': 'lo de la relatividad y eso',
+    'texto': 'Hay dos cosas que son infinitas: el universo y la estupidez humana, de la primera no estoy muy seguro',
   },
   {
-    'autor': 'Albert Einsen',
-    'texto': 'te salgo hasta en la sopa',
+    'autor': 'Mahatma Gandhi',
+    'texto': 'El nacimiento y la muerte no son dos estados distintos, sino dos aspectos del mismo estado',
   },
   {
-    'autor': 'd',
-    'texto': 'Ay dias q tiras la carnada y pica al momento, dias q demora,y dias q no pica ,no estoy hablando de pescar',
+    'autor': 'Peter Bamm',
+    'texto': 'Lo que importa verdaderamente en la vida no son los objetivos que nos marcamos, sino los caminos que seguimos para lograrlo ',
   },
   {
-    'autor': 'c',
-    'texto': 'Ay dias q tiras la carnada y pica al momento, dias q demora,y dias q no pica ,no estoy hablando de pescar',
+    'autor': 'Benjamin Franklind',
+    'texto': 'La pereza viaja tan despacio que la pobreza no tarda en alcanzarla',
   },
   {
-    'autor': 'cc',
-    'texto': 'Ay dias q tiras la carnada y pica al momento, dias q demora,y dias q no pica ,no estoy hablando de pescar',
+    'autor': 'John Steinbeck',
+    'texto': 'De todos los animales de la creación el hombre es el único que bebe sin tener sed, come sin tener hambre y habla sin tener nada que decir',
   },
   {
-    'autor': 'ccc',
-    'texto': 'Ay dias q tiras la carnada y pica al momento, dias q demora,y dias q no pica ,no estoy hablando de pescar',
+    'autor': 'Marco Tulio Cicerón',
+    'texto': 'De humanos es errar y de necios permanecer en el error ',
+  },
+  {
+    'autor': 'Stephen Hawking',
+    'texto': 'Incluso la gente que afirma que no podemos hacer nada para cambiar nuestro destino, mira antes de cruzar la calle ',
   },
 ]
